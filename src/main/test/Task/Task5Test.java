20d9dcf40e74e95c0cb048dd6e6dc1fd229c901d
Anhvtq3 <anhvtq3@onemount.com>
2021-07-23 12:16:50 +0700

    submit
    submit

---
+++ src/main/test/Task/Task5Test.java
@@ -0,0 +1,12 @@
+package Task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Task5Test {
+
+    @Test
+    void removeAllNumbersGreaterThan10() {
+    }
+}
